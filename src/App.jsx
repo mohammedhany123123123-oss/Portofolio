f10912c6668df8a5757c0f39709923dbfc577f5f
Mohammed Hany <delllabtop96@gmail.com>
2026-06-13 14:48:29 +0300

    Fix MySkills component naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import Aboutme from './Components/Aboutme/Aboutme'
-import Myskills from './Components/Myskills/Myskills'
-import Myprojects from './Components/Myprojects/Myprojects'
-import Contactme from './Components/Contactme/Contactme'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Aboutme from "./Components/Aboutme/Aboutme";
+import MySkills from "./Components/Myskills/MySkills";
+import Myprojects from "./Components/Myprojects/Myprojects";
+import Contactme from "./Components/Contactme/Contactme";
 import { Routes, Route } from "react-router-dom";
 
 <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/about" element={<Aboutme />} />
-  <Route path="/skills" element={<Myskills />} />
+  <Route path="/skills" element={<MySkills />} />
   <Route path="/projects" element={<Myprojects />} />
   <Route path="/contact" element={<Contactme />} />
-</Routes>
+</Routes>;
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Aboutme/>
-      <Myskills/>
-      <Myprojects/>
-      <Contactme/>
+      <Navbar />
+      <Home />
+      <Aboutme />
+      <MySkills />
+      <Myprojects />
+      <Contactme />
     </div>
-  )
+  );
 }
